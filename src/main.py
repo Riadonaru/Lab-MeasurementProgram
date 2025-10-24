c36ee45a978021415b2d7be826e6fd234406201f
Riadonaru <Liad101112@gmail.com>
2025-10-24 21:36:03 +0300

    added logger setup and some logging

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
-from setup import generate_waveform
+from setup import generate_waveform, set_logger
+
 
 def main() -> None:
+    # logger setup
+    set_logger()
+    
     # load experiment settings
     generate_waveform(plot=True)
     
--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import logging
 
 from globals import SETTINGS
+
+
+def set_logger() -> None:
+    """Set up the logger for the application."""
+    log_file = os.path.join(os.getcwd(), "logs\\app.log")
+
+    logging.basicConfig(
+        level=logging.INFO,                 # Minimum level to log
+        format="[%(asctime)s] - %(levelname)s : %(message)s",
+        handlers=[
+            logging.FileHandler(log_file), # Log to file
+        ]
+    )
+    logging.info("Logger initialized.")
+    return
+
 
 def generate_waveform(plot: bool = False) -> np.ndarray:
     """Generate and plot a Gaussian pulse waveform based on settings."""
@@ -14,6 +32,9 @@ def generate_waveform(plot: bool = False) -> np.ndarray:
 
     x = np.linspace(-5, 5, 500)
     y = A * np.exp(-((x - t0)**2) / (2 * sigma**2)) * np.cos(2 * np.pi * f * x + phi)
+    
+    logging.info(f"Generated gaussian pulse with parameters: Amplitude: {A} Frequency: {f} Phase: {phi} Offset: {t0} Width: {sigma}")
+    
     if plot:
         plt.plot(x, y)
         plt.title('Gaussian Pulse Waveform')
@@ -23,3 +44,5 @@ def generate_waveform(plot: bool = False) -> np.ndarray:
         plt.show()
 
     return y
+
+
